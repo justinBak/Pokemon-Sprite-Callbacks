750e708c81d61f351681fc4b8a75a14b575f2fc5
Justin Baker <100954109+justinBak@users.noreply.github.com>
2022-04-12 08:49:24 -0400

    Modify app.js, index.html, and styles.css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,18 @@
 const container = document.querySelector('.container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-const btn = document.querySelector('.button');
-const pokemon = document.createElement('div');
+const submit = document.querySelector('.button');
 const newSprite = document.createElement('img');
 const label = document.createElement('span');
 
 // Randomly Generated Pokemon Sprites
-btn.addEventListener('click', () => {
+submit.addEventListener('click', () => {
   // Generates a random collection from 800 pokemon
   let randNum = Math.floor(Math.random() * 800);
   //Loops through and prints one pokemon from the list of 800 Pokemon characters everytime user clicks button
   for(i = 1; i < randNum; ++i){
-    pokemon.classList.add('pokemon'); // <---- Adds multiple classes of 
     label.innerText = `#${i}`;
     newSprite.src = `${baseURL}${i}.png`; // <---- Image source; adds pokemon sprite number
     container.appendChild(newSprite); // <---- Appends image to container div
-    container.appendChild(label);
+    container.appendChild(label); // <---- Appends span to container div
   }
 })
